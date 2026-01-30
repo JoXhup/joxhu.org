3116cf6c93929b4df8c1e7eb0046a0440309196b
Joshua <joshuamoranvar@gmail.com>
2026-01-29 20:13:36 -0600

    FIX

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/joxhu.org/",
+    base: "/",
     plugins: [react()],
     resolve: {
         alias: {
